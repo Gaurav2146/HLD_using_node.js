d181418876878cb14ed08229a68b0f5040304894
gaurav2146 <gaurav2146.gk@gmail.com>
2024-02-15 23:13:30 +0530

    chnages done

--- src/main_thread.ts
+++ src/main_thread.ts
@@ -8,12 +8,6 @@ if (isMainThread) {
 
   let res_path = path.join(__dirname,"Child","worker_thread.js");
 
-  console.log("======================================================");
-  console.log(__dirname,"directory name");
-  console.log(__filename,"__file name");
-  console.log(res_path,"res_path");
-  console.log("======================================================");
-
   // Create a new worker thread
   const worker = new Worker(res_path);
 
@@ -24,16 +18,4 @@ if (isMainThread) {
 
   // Send a message to the worker
   worker.postMessage('Hello from main thread!');
-} else {
-  // This code runs in the worker thread
-
-  console.log('Worker thread starting...');
-
-  // Listen for messages from the main thread
-  parentPort?.on('message', message => {
-    console.log('Message from main thread:', message);
-
-    // Send a message back to the main thread
-    parentPort?.postMessage('Hello from worker thread!');
-  });
 }
